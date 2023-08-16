93104581ee208f7936a7419987af9144f22f1ce1
MeshachArinze <meshacharinze@gmail.com>
2023-08-16 10:30:44 +0100

    commit to main branch

---
+++ fullstackblog/client/src/pages/register.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Register = () => {
+  return <div>Register</div>;
+};
+
+export default Register;
