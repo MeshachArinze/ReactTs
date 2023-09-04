6eeb8555c7f97fffa7152242533628010afa37de
MeshachArinze <meshacharinze@gmail.com>
2023-09-04 11:05:55 +0100

    commto to changes

---
+++ multistepform/src/components/step -1.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Formik, Field, ErrorMessage } from "formik";
+import validationSchema from "./validationSchema";
+
+interface Step1Props {
+  formData: any;
+  setFormData: React.Dispatch<React.SetStateAction<any>>;
+  nextStep: () => void;
+}
+
+const Step1: React.FC<Step1Props> = ({ formData, setFormData, nextStep }) => {
+  const handleNext = () => {
+    nextStep(); // Move to the next step
+  };
+
+  return (
+    <div>
+      <h2>Step 1: Personal Information</h2>
+      <Formik
+        initialValues={formData}
+        validationSchema={validationSchema}
+        onSubmit={(values: any) => {
+          setFormData(values);
+          handleNext();
+        }}
+      >
+        <form>
+          <div>
+            <label>First Name:</label>
+            <Field type="text" name="firstName" />
+            <ErrorMessage name="firstName" component="div" className="error" />
+          </div>
+          {/* Add more form fields for Step 1 */}
+          <button type="button" onClick={handleNext}>
+            Next
+          </button>
+        </form>
+      </Formik>
+    </div>
+  );
+};
+
+export default Step1;
