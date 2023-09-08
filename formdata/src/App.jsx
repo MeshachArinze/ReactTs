42d20ccee36b05f3e827a353f0ef3e8f30f3a4e6
MeshachArinze <meshacharinze@gmail.com>
2023-09-08 17:59:29 +0100

    commit to main

--- formdata/src/App.jsx
+++ formdata/src/App.jsx
@@ -1,46 +1,13 @@
-import { useEffect, useRef } from "react";
 
-export default function FormWithoutState() {
-  // The count will increment by 2 on initial render due to strict mode then by 1 on subsequent renders
-  const countRef = useRef(0);
+import FormWithoutState from './formdata'
 
-  useEffect(() => {
-    countRef.current = countRef.current + 1;
-  });
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    const form = new FormData(e.currentTarget);
-    const email = form.get("email");
-    const password = form.get("password");
-    console.log({ email, password });
-    const body = {};
-    for (const [key, value] of form.entries()) {
-      body[key] = value;
-    }
-    console.log(body);
-    // Do Further input validation and submit the form
-  }
+function App() {
 
   return (
-    <div className="form-div">
-      <h2>Form Without State</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="input-field">
-          <label htmlFor="email1">Email</label>
-          <input id="email1" type="email" name="email" autoComplete="off" />
-        </div>
-        <div className="input-field">
-          <label htmlFor="password1">Password</label>
-          <input id="password1" type="password" name="password" />
-        </div>
-        <button type="submit">Submit</button>
-        <div>
-          <p>
-            The Component Re-Rendered <span>{countRef.current}</span> times
-          </p>
-        </div>
-      </form>
-    </div>
-  );
+    <>
+      <FormWithoutState />
+    </>
+  )
 }
+
+export default App
